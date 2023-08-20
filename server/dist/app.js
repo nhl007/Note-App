4694ed7566baaecc0377c72626055b089f77d5fe
Nihal <apar.asif.an@gmail.com>
2023-08-20 06:33:10 +0600

    new dep

--- server/dist/app.js
+++ server/dist/app.js
@@ -1,29 +1,30 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const noteRoute_1 = __importDefault(require("./routes/noteRoute"));
-const authRoute_1 = __importDefault(require("./routes/authRoute"));
-const error_1 = require("./middleware/error");
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
+const cors_1 = __importDefault(require('cors'));
+const noteRoute_1 = __importDefault(require('./routes/noteRoute'));
+const authRoute_1 = __importDefault(require('./routes/authRoute'));
+const error_1 = require('./middleware/error');
 // import helmet from 'helmet';
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cookie_parser_1 = __importDefault(require('cookie-parser'));
 const app = (0, express_1.default)();
 const whitelist = process.env.WHITELIST;
 const corsOptions = {
-    credentials: true,
-    origin: (origin, callback) => {
-        // `!origin` allows server-to-server requests (ie, localhost requests)
-        if (!origin || whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error('Not allowed by CORS: ' + origin));
-        }
-    },
-    optionsSuccessStatus: 200,
+  credentials: true,
+  origin: (origin, callback) => {
+    // `!origin` allows server-to-server requests (ie, localhost requests)
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS: ' + origin));
+    }
+  },
+  optionsSuccessStatus: 200,
 };
 app.use((0, cors_1.default)(corsOptions));
 // app.disable('x-powered-by');
@@ -31,16 +32,17 @@ app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json({ limit: '50mb' }));
 app.use(express_1.default.urlencoded({ limit: '50mb', extended: false }));
 app.use((0, cookie_parser_1.default)());
+
 app.get('/', (req, res) => {
-    res.json({
-        status: 'running',
-        message: 'Server is running !',
-    });
+  res.json({
+    status: 'running',
+    message: `${process.env.ENVIRONMENT}+"env and db"+${process.env.MONGO_PROD_URL}`,
+  });
 });
 app.use('/api/v1/auth', authRoute_1.default);
 app.use('/api/v1/notes', noteRoute_1.default);
 app.use((req, res, next) => {
-    res.status(404).send('No route found!');
+  res.status(404).send('No route found!');
 });
 app.use(error_1.handleErrors);
 exports.default = app;
